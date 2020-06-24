63ab443cb71d51913fd4fd10af4cf4eeb1ef57d8
Yuna Lee <carnivalss@naver.com>
2020-06-24 15:04:02 +0900

    Modify Loader

--- assets/js/loading.js
+++ assets/js/loading.js
@@ -1,21 +1,21 @@
-// document.onreadystatechange = function () {
-//     var state = document.readyState
-//     if (state == 'complete') {
-//            document.getElementById('interactive');
-//            document.querySelector('.loading').style.display="none";
-//     }
-// }
-
 document.onreadystatechange = function () {
-    const state = document.readyState
-
-    if (state == "interactive") {
-        document.getElementById("jsMain").style.display = "none";
-    } else if (state == "complete") {
-        setTimeout(function () {
-            document.getElementById('interactive');
-            document.querySelector('.loading').style.display="none";
-            document.getElementById("jsMain").style.display = "block";
-        }, 1000)
+    var state = document.readyState
+    if (state == 'complete') {
+           document.getElementById('interactive');
+           document.querySelector('.loading').style.display="none";
     }
 }
+
+// document.onreadystatechange = function () {
+//     const state = document.readyState
+
+//     if (state == "interactive") {
+//         document.getElementById("jsMain").style.display = "none";
+//     } else if (state == "complete") {
+//         setTimeout(function () {
+//             document.getElementById('interactive');
+//             document.querySelector('.loading').style.display="none";
+//             document.getElementById("jsMain").style.display = "block";
+//         }, 1000)
+//     }
+// }
