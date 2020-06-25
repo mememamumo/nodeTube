4c20e27d2dfdfd1fc89b18cc12c7d3b58a6ff750
Yuna Lee <carnivalss@naver.com>
2020-06-25 23:48:40 +0900

    @BABEL

--- src/db.js
+++ src/db.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 mongoose.connect(
-    process.env.PRODUCTION ? process.env.MONGO_URL_PROD : process.env.MONGO_URL,
+    process.env.MONGO_URL_PROD,
 {
     useNewUrlParser: true,
     useFindAndModify: false
